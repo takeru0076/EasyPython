9c836730efdd7cd5eacc7dcce0dc9e3a379dc79f
takeru0076 <takeru200276@icloud.com>
2022-07-16 20:20:24 +0900

    ファイルから取得

--- pybot.py
+++ pybot.py
@@ -1,11 +1,11 @@
-while True:
-    command = input('pybot> ')
-    if 'こんにちは' in command:
-        print("コンニチハ")
-    elif 'ありがとう' in command:
-        print("ドウイタシマシテ")
-    elif 'さようなら' in command:
-        print("サヨウナラ")
-        break
-    else:
-        print("ナニヲイッテルノカワカラナイ")
+command_file = open("pybot.txt", encoding="utf-8")
+raw_data = command_file.read()
+command_file.close()
+lines = raw_data.spliitlines()
+ 
+ bot_dict = {}
+ for line in lines:
+     word_list = line.split(",")
+     key = word_list[0]
+     responnse = word_list[1]
+     bot_dict[key] = response
