f42b79749d2a2bea6baef3495476e74348caf129
takeru0076 <takeru200276@icloud.com>
2022-07-16 20:38:19 +0900

    use function

--- pybot.py
+++ pybot.py
@@ -1,3 +1,13 @@
+def heisei_command(command):   
+    heisei, year_str = command.split()
+    year = int(year_str)
+    if year >= 1989:
+        heisei_year = year - 1988
+        response = '西暦{}年ハ,平成{}年デス'.format(year,heisei_year)
+    else:
+        response = '西暦{}年ハ、平成デハアリマセン'.format(year)
+    return response    
+    
 command_file = open("pybot.txt", encoding="utf-8")
 raw_data = command_file.read()
 command_file.close()
@@ -17,6 +27,9 @@ while True:
         if message in command:
             response = bot_dict[message]
             break
+        if "平成" in command:
+            response = heisei_command(command)
+                
     
     if not response:
         response = 'ナニヲイッテルノカワカラナイ'
