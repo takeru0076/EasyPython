2a5ae1daac46eb13fe6a20bce1e5803b3b3167ff
takeru0076 <takeru200276@icloud.com>
2022-07-22 18:57:29 +0900

    use wikipedia パッケージ

---
+++ pybot_wikipedia.py
@@ -0,0 +1,13 @@
+import wikipedia
+
+def wikipedia_command(command):
+    cmd, keyword = command.split(maxsplit=1)
+    wikipedia.set_lang('ja')
+    try:
+        page = wikipedia.page(keyword)
+        title = page.title
+        summary = page.summary
+        response = 'タイトル: {}\n{}'.format(title, summary)
+    except wikipedia.exceptions.PageError:
+        response = '「{}」ノ意味ガ見ツカリマセンデシタ'.format(keyword)
+    return response
